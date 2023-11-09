61178b2ddfc8591c07cef2495e63abf3349db5f1
Denys Niestierov <niestierov.denys.natife@gmail.com>
2023-11-09 20:47:57 +0200

    added uiviewcontroller extension

--- NearestPlaces/Extensions/UIViewController+Extension.swift
+++ NearestPlaces/Extensions/UIViewController+Extension.swift
@@ -5,4 +5,26 @@
 //  Created by Denys Niestierov on 09.11.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    typealias EmptyBlock = () -> Void
+    typealias AlertButtonAction = (title: String, completion: EmptyBlock?)
+
+    func showAlert(title: String, message: String, actions: [AlertButtonAction]?) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        actions?.forEach { action in
+            let alertAction = UIAlertAction(title: action.title, style: .default) { _ in
+                action.completion?()
+            }
+            alertController.addAction(alertAction)
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
